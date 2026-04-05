41c5bfa6da3e22dd0e6bf8413c794b13805da16c
Filipe Mota Pinho <filipe.mota@gmail.com>
2026-04-05 16:11:35 -0300

    Start

--- app/Views/Home/index.php
+++ app/Views/Home/index.php
@@ -1,7 +1,19 @@
 <?= $this->extend('layout/main') ?>
 
+<?= $this->section('title') ?>
+    <?=  $title; ?>
+<?= $this->endSection() ?>
+
+<?= $this->section('css') ?>
+
+<?= $this->endSection() ?>
+
 <?= $this->section('content') ?>
 
 <!-- TODO O SEU HTML AQUI -->
+
+<?= $this->endSection() ?>
+
+<?= $this->section('js') ?>
 
 <?= $this->endSection() ?>
